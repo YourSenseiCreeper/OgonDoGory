8523ced75ddf636bcd46a739d5d88f37344d4ef9
YourSenseiCreeper <tomanycreeps@gmail.com>
2021-05-08 13:44:56 +0200

    Po przejściu do innego miejsca scroll przenosi się do góry

--- src/app/root-routing.module.ts
+++ src/app/root-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', scrollPositionRestoration: 'enabled'})],
     exports: [RouterModule],
     providers: []
 })
